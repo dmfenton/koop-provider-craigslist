7ad2c9290c58bf4899818ddf3645def3291aabd7
dmfenton <dfenton@esri.com>
2017-02-14 08:23:21 -0500

    allow custom port on server implementation

--- server/index.js
+++ server/index.js
@@ -11,5 +11,7 @@ const craigslist = require('../')
 koop.register(craigslist)
 
 // Start listening for http traffic
-koop.server.listen(8080)
-console.log('Koop Craigslist listening on 8080')
+const config = require('config')
+const port = config.port || 8080
+koop.server.listen(port)
+console.log(`Koop Craigslist listening on ${port}`)
