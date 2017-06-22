4470a7d582d345b614ac4e2481757f4a9846afaa
dmfenton <dfenton@esri.com>
2017-06-22 18:17:49 -0400

    fix url to posting

--- model.js
+++ model.js
@@ -1,46 +1,48 @@
-'use strict'
+'use strict';
 // Cache configured time or 1 hour
-const config = require('config')
-const ttl = (config.craigslist && config.craigslist.ttl) || 60 * 60
-const request = require('request').defaults({gzip: true})
-const types = require('./mappings/types.js')
+const config = require('config');
+const ttl = (config.craigslist && config.craigslist.ttl) || 60 * 60;
+const request = require('request').defaults({ gzip: true });
+const types = require('./mappings/types.js');
 
-module.exports = function () {
+module.exports = function() {
   // This is our one public function it's job its to fetch data from craigslist and return as a feature collection
-  this.getData = function (req, callback) {
-    const city = req.params.host
-    const type = req.params.id
+  this.getData = function(req, callback) {
+    const city = req.params.host;
+    const type = req.params.id;
     request(`https://${city}.craigslist.org/jsonsearch/${types[type]}/?map=1`, (err, res, body) => {
-      if (err) return callback(err)
-      const apartments = translate(res.body)
-      apartments.ttl = ttl
+      if (err) return callback(err);
+      const apartments = translate(res.body);
+      apartments.ttl = ttl;
       apartments.metadata = {
         name: `${city} ${type}`,
         description: `Craigslist ${type} listings proxied by https://github.com/dmfenton/koop-provider-craigslist`
-      }
-      callback(null, apartments)
-    })
-  }
-}
+      };
+      callback(null, apartments);
+    });
+  };
+};
 
 // Map accross all elements from a Craigslist respsonse and translate it into a feature collection
-function translate (data) {
-  const list = JSON.parse(data)
+function translate(data) {
+  const list = JSON.parse(data);
   const featureCollection = {
     type: 'FeatureCollection',
     features: []
-  }
+  };
   if (list && list[0]) {
-    const apartments = list[0].filter(node => { return node.Ask })
-    featureCollection.features = apartments.map(formatFeature)
+    const apartments = list[0].filter(node => {
+      return node.Ask;
+    });
+    featureCollection.features = apartments.map(formatFeature);
   }
-  return featureCollection
+  return featureCollection;
 }
 
 // This function takes a single element from the craigslist response and translates it to GeoJSON
 // TODO format based on schema types for other craiglists things like jobs
-function formatFeature (apt) {
-  const feature =  {
+function formatFeature(apt) {
+  const feature = {
     type: 'Feature',
     geometry: {
       type: 'Point',
@@ -51,17 +53,17 @@ function formatFeature (apt) {
       price: parseFloat(apt.Ask),
       bedrooms: parseFloat(apt.Bedrooms),
       postDate: dateFormat(apt.PostedDate),
-      posting: 'https:' + apt.PostingURL,
+      posting: apt.PostingURL,
       thumbnail: apt.ImageThumb
     }
+  };
+  if (!isNaN(feature.properties.price) && !isNaN(feature.properties.bedrooms)) {
+    const ppbr = feature.properties.price / feature.properties.bedrooms;
+    if (ppbr !== 0 && ppbr !== Infinity) feature.properties.pricePerBedroom = ppbr;
   }
- if (!isNaN(feature.properties.price) && !isNaN(feature.properties.bedrooms)) {
-   const ppbr = feature.properties.price / feature.properties.bedrooms
-   if (ppbr !== 0 && ppbr !== Infinity) feature.properties.pricePerBedroom = ppbr
- }
- return feature
+  return feature;
 }
 
-function dateFormat (date) {
-  return new Date(parseInt(date, 10) * 1000).toISOString()
+function dateFormat(date) {
+  return new Date(parseInt(date, 10) * 1000).toISOString();
 }
