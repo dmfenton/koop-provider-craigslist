c7b173e61c198ab789bd5f44ad5434c06ef4a326
Rich Gwozdz <rich.gwozdz@gmail.com>
2018-05-08 13:51:31 -0700

    Add route-exclusion to token validation middleware.

--- middleware/index.js
+++ middleware/index.js
@@ -2,24 +2,39 @@ const jwt = require('jsonwebtoken')
 const unless = require('express-unless')
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET
 
-const tokenValidator = function(req, res, next) {
-  console.log('token-middleware')
-  const tokenResponse = { 
-    "error": {
-      "code": 499,
-      "message": "Token Required",
-      "details": []
-    }
-  }
-  if (req.query.token === undefined) return res.status(200).json(tokenResponse)
+/**
+*
+*/
+function tokenValidator (providerName) {
+  
+  // Define validation function
+  let validationFunc = function(req, res, next) {
 
-  jwt.verify(req.query.token, ACCESS_TOKEN_SECRET, function(err, decoded) {
-    if (err) next(err)
-    next()
+    // Verify token
+    jwt.verify(req.query.token, ACCESS_TOKEN_SECRET, function(err, decoded) {
+      // Respond with success, but send 499 error object (AGOL spec)
+      if (err) return res.status(200).json({ 
+        "error": {
+          "code": 499,
+          "message": "Token Required",
+          "details": []
+        }
+      }) 
+      // Otherwise, move on to next middleware
+      return next()
+    });
+  }
+
+  // Add "unless" to validation function to exclude specific routes from getting this middleware
+  validationFunc.unless = unless
+  return validationFunc.unless({
+    path: [
+      {url: `/${providerName}/rest/info`, methods: ['GET', 'POST']},
+      {url: `/${providerName}/generateToken`, methods: ['GET', 'POST']}
+    ]
   });
 }
 
-tokenValidator.unless = unless;
 
 module.exports = { tokenValidator }
 
